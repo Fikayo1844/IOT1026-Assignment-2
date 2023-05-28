038b16aa8cecba43df66d56a50ca9a9bf77aa777
Fikayo1844 <fikayoduyile01@gmail.com>
2023-05-28 05:07:25 -0400

    Program implementation

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,6 +5,9 @@
         static void Main()
         {
             System.Console.WriteLine("Hello World");
+            TreasureChest chest = new TreasureChest(TreasureChest.State.Closed);
+            chest.Manipulate(TreasureChest.Action.Open);
+            System.Console.WriteLine(chest);
         }
     }
 }
