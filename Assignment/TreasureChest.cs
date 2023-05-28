c824497e4e7bf8a96ceab793cb4fa03184159e0c
Fikayo1844 <fikayoduyile01@gmail.com>
2023-05-28 05:05:44 -0400

    TC Implementation

--- Assignment/TreasureChest.cs
+++ Assignment/TreasureChest.cs
@@ -1,6 +1,6 @@
 ﻿namespace Assignment
 {
-     public class TreasureChest
+    public class TreasureChest
     {
         private State _state = State.Locked;
         private readonly Material _material;
@@ -8,8 +8,9 @@
         private readonly LootQuality _lootQuality;
 
         // Default Constructor
+        public TreasureChest()
         {
-                   _material = Material.Iron;
+            _material = Material.Iron;
             _lockType = LockType.Expert;
             _lootQuality = LootQuality.Green;
         }
@@ -21,37 +22,75 @@
         }
 
         public TreasureChest(Material material, LockType lockType, LootQuality lootQuality)
-        { 
+        {
             _material = material;
             _lockType = lockType;
             _lootQuality = lootQuality;
         }
 
-        public State? Manipulate(Action action)
-         // This is called a getter
+        // This is called a getter
         public State GetState()
         {
-           return _state;
+            return _state;
         }
-        
-         public State Manipulate(Action action)
-         {
-              if (action == Action.Open) {
-                  Open();
+
+        public State Manipulate(Action action)
+        {
+            switch (action)
+            {
+                case Action.Open:
+                    Open();
+                    break;
+                case Action.Close:
+                    Close();
+                    break;
+                case Action.Lock:
+                    Lock();
+                    break;
+                case Action.Unlock:
+                    Unlock();
+                    break;
+                default:
+                    Console.WriteLine("Invalid action");
+                    break;
             }
             return _state;
-         }
-
+        }
         public void Unlock()
         {
-            throw new NotImplementedException();
-        }
-        public void Lock()
-        {
-            throw new NotImplementedException();
+            // We should check if the chest is Locked
+            if (_state == State.Locked)
+            {
+                _state = State.Closed;
+            }
+            else if (_state == State.Closed)
+            {
+                Console.WriteLine("The chest needs to be Locked!");
+            }
+            else
+            {
+                Console.WriteLine("The chest needs to be closed , then locked.");
+            }
         }
 
-         public void Open()
+        public void Lock()
+        {
+            // We should check if the chest is Closed
+            if (_state == State.Closed)
+            {
+                _state = State.Locked;
+            }
+            else if (_state == State.Locked)
+            {
+                Console.WriteLine("The chest is already Locked!");
+            }
+            else
+            {
+                Console.WriteLine("Close the Chest.");
+            }
+        }
+
+        public void Open()
         {
             // We should check if the chest is closed
             if (_state == State.Closed)
@@ -68,9 +107,21 @@
             }
         }
 
-        private void Close()
+        public void Close()
         {
-            throw new NotImplementedException();
+            // We should check if the chest is open
+            if (_state == State.Open)
+            {
+                _state = State.Closed;
+            }
+            else if (_state == State.Closed)
+            {
+                Console.WriteLine("The chest is already closed!");
+            }
+            else if (_state == State.Locked)
+            {
+                Console.WriteLine("The chest cannot be closed because it is  already locked.");
+            }
         }
 
         public override string ToString()
